f5f42d99b113f637a988f765c9f98ef52547ccd7
amaxilat <d.amaxilatis@sparkworks.net>
2018-06-23 15:08:46 +0300

    check inputs

--- html/ac/action.php
+++ html/ac/action.php
@@ -1,4 +1,12 @@
 <?php
-        exec("irsend SEND_ONCE ".$_GET['device']." ".$_GET['command']);
-        header('Location: ./');
-?>
+$devices = array("ytf1", "bn59");
+$commands = array("on", "off");
+
+if (isset($_GET['device']) && isset($_GET['command'])) {
+    $device = $_GET['device'];
+    $command = $_GET['command'];
+    if (in_array($device, $devices) && in_array($command, $commands)) {
+        exec("irsend SEND_ONCE " . $_GET['device'] . " " . $_GET['command']);
+    }
+}
+header('Location: ./');
