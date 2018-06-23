092353aaaef681e17bbe41501ad1781576f602f9
amaxilat <d.amaxilatis@sparkworks.net>
2018-06-23 15:10:31 +0300

    use constants

--- html/ac/action.php
+++ html/ac/action.php
@@ -1,12 +1,15 @@
 <?php
+define("DEVICE", "device");
+define("COMMAND", "command");
+
 $devices = array("ytf1", "bn59");
 $commands = array("on", "off");
 
-if (isset($_GET['device']) && isset($_GET['command'])) {
-    $device = $_GET['device'];
-    $command = $_GET['command'];
+if (isset($_GET[DEVICE]) && isset($_GET[COMMAND])) {
+    $device = $_GET[DEVICE];
+    $command = $_GET[COMMAND];
     if (in_array($device, $devices) && in_array($command, $commands)) {
-        exec("irsend SEND_ONCE " . $_GET['device'] . " " . $_GET['command']);
+        exec("irsend SEND_ONCE " . $device . " " . $command);
     }
 }
 header('Location: ./');
