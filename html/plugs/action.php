43fff72883c89e9bc2952e98019952f2f1ca0a4c
amaxilat <d.amaxilatis@sparkworks.net>
2018-08-26 17:57:11 +0300

    add support for hue lamps

--- html/plugs/action.php
+++ html/plugs/action.php
@@ -2,11 +2,14 @@
 define("PLUG", "plug");
 define("STATUS", "status");
 define("YEE", "yee");
+define("HUE", "hue");
 define("YEE_SERVER", "http://192.168.1.10:8082/v1");
+define("HUE_SERVER", "http://192.168.1.30/api/token/groups/3/action");
 
 $plugs = array("1", "2", "3", "4");
-$statuses = array(0, 1);
+$statuses = array(0, 1, "energize", "concentrate", "relax", "read", "dimmed");
 $yees = array("0x0000000000c936d0");
+$hues = array("3");
 
 if (isset($_GET[PLUG]) && isset($_GET[STATUS])) {
     $plug = $_GET[PLUG];
@@ -32,4 +35,38 @@ if (isset($_GET[YEE]) && isset($_GET[STATUS])) {
         curl_close($ch);
     }
 }
+if (isset($_GET[HUE]) && isset($_GET[STATUS])) {
+    $hue = $_GET[HUE];
+    $status = $_GET[STATUS];
+    if (in_array($hue, $hues) && in_array($status, $statuses)) {
+        $ch = curl_init();
+        $data = array();
+        if ($status == 1) {
+            $data = array("on" => true, "bri" => 254, "hue" => 34069, "sat" => 251);
+        } else if ($status == "energize") {
+            $data = array("on" => true, "bri" => 254, "hue" => 34069, "sat" => 251);
+        } else if ($status == "concentrate") {
+            $data = array("on" => true, "bri" => 254, "hue" => 32938, "sat" => 50);
+        } else if ($status == "relax") {
+            $data = array("on" => true, "bri" => 144, "hue" => 13401, "sat" => 204);
+        } else if ($status == "read") {
+            $data = array("on" => true, "bri" => 254, "hue" => 15338, "sat" => 121);
+        } else if ($status == "dimmed") {
+            $data = array("on" => true, "bri" => 77, "hue" => 14988, "sat" => 141);
+        } else if ($status == 0) {
+            $data = array("on" => false);
+        }
+
+        $data_json = json_encode($data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+        curl_setopt($ch, CURLOPT_URL, HUE_SERVER);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data_json)));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+    }
+}
+
 header('Location: ./');
